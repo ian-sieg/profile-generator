a87d7636da02c8e20fbc265aeffbf3ad5f65f481
ian.moss <ianmsieg@gmail.com>
2022-06-14 15:20:51 -0500

    added path and linked html generate js file to index

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const inquirer = require('inquirer')
+const printPage = require('./src/htmlGen')
+
 const Manager = require('./lib/manager')
 const Engineer = require('./lib/engineer')
 const Intern = require('./lib/intern')
-const fs = require('fs')
 
 const staff = []
 
@@ -13,7 +14,7 @@ inquirer
             type: 'list',
             name: 'role',
             message: "What is the team member's role?",
-            choices: ['Intern', 'Engineer', 'Manager'],
+            choices: ['Intern', 'Engineer'],
         },
         {
             name: 'name',
@@ -56,10 +57,8 @@ inquirer
         let special = '';
         if (role === 'Intern') {
             special = 'school name'
-        } else if (role === 'Engineer') {
-            special = 'GitHub username'
         } else {
-            special = 'office number'
+            special = 'GitHub username'
         };
 
         inquirer
@@ -78,21 +77,84 @@ inquirer
             .then (({special, more}) => {
                 let newStaff;
                 if (role === 'Intern') {
-                    newStaff = new Intern(name, email, id, special)
-                } else if (role === 'Engineer') {
-                    newStaff = new Engineer(name, email, id, special)
+                    newStaff = new Intern(role, name, email, id, special)
                 } else {
-                    newStaff = new Manager(name, email, id, special)
+                    newStaff = new Engineer(role, name, email, id, special)
                 };
 
                 staff.push(newStaff)
-                console.log(staff)
-                more === 'Yes' ? anotherStaff() : printPage();
+                more === 'Yes' ? anotherStaff() : printPage(staff);
             })
     })
 }
 
-anotherStaff()
+function teamBoss() {
+    inquirer
+        .prompt([
+            {
+                name: 'name',
+                message: "What is the team manager's name?",
+                validate: (ans) => {
+                    if(ans !== '') {
+                        return true;
+                    } else {
+                        console.error('Their name cannot be blank');
+                        return false;
+                    };
+                },
+            },
+            {
+                name: 'email',
+                message: 'What is their email address?',
+                validate: (ans) => {
+                    if(ans !== '') {
+                        return true;
+                    } else {
+                        console.error('Their email cannot be blank');
+                        return false;
+                    };
+                },
+            },
+            {
+                name: 'id',
+                message: 'What is their ID number?',
+                validate: (ans) => {
+                    if(ans !== '') {
+                        return true;
+                    } else {
+                        console.error('Their ID cannot be blank');
+                        return false;
+                    };
+                },
+            },
+            {
+                name: 'office',
+                message: 'What is their office number?',
+                validate: (ans) => {
+                    if(ans !== '') {
+                        return true;
+                    } else {
+                        console.error('Their office number cannot be blank');
+                        return false;
+                    };
+                },
+            },
+            {
+                type: 'list',
+                name: 'newMem',
+                message: "Would you like to add more team members or finish building the team?",
+                choices: ['Add more team members', 'Finished'],
+            }
+        ])
+        .then(({role, name, email, id, office, newMem}) => {
+            let newMngr = new Manager(role, name, email, id, office)
+            staff.push(newMngr)
+
+            newMem === 'Finished' ? printPage(staff) : anotherStaff();
+        })
+}
+
+teamBoss();
 
 //TODO
 // add printPage function to create the actual HTML with input from staff array
---
+++ src/htmlGen.js
@@ -0,0 +1,7 @@
+const fs = require('fs')
+
+function printPage(staff) {
+    console.log(staff)
+}
+
+module.exports = printPage
